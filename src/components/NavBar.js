52f47ca03abef177f8c9dc919d7f8c55681bd267
aar.56aint <aaron.hays_7@yahoo.com>
2020-07-04 15:53:36 +0100

    reconfigured eslint/airbnb, created navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 import logo from '../images/logo.png';
 
@@ -6,6 +7,17 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <img src={logo} alt="estate-logo" />
+
+      <ul className="navbar-links">
+        <li className="navbar-link-item">
+          <Link to="url">View Property</Link>
+
+        </li>
+
+        <li>
+          <Link to="url">Add Property</Link>
+        </li>
+      </ul>
     </div>
   );
 };

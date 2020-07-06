03c2bb4c43785116e8bcc08dcfdb34edfe9c4e94
aar.56aint <aaron.hays_7@yahoo.com>
2020-07-07 00:47:04 +0100

    created controlled components for AddProperty component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/AddProperty.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
     <>
-      <div>Add Property Page</div>
+      <div className="add-property">
+        <form onSubmit={handleAddProperty}>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+          </label>
+
+          <button type="submit" className="add-property-button">
+            <span>Add</span>
+          </button>
+        </form>
+      </div>
     </>
   );
 };

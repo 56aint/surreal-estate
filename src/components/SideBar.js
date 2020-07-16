c9ca3bbf73e7d9a53ef2ecf19aec8eabe6257c8c
aar.56aint <aaron.hays_7@yahoo.com>
2020-07-16 17:45:39 +0100

    select menu options changed to object, working now, & working on search capability for SideBar component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,7 +4,7 @@ import { Link, useLocation, useHistory } from "react-router-dom";
 import qs from "qs";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../styles/SideBar.css";
+// import "../styles/SideBar.css";
 
 const SideBar = () => {
   const { search } = useLocation();
